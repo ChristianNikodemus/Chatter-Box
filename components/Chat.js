52689915f410a3c73165889539f5b278a6543c70
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-12-18 12:01:44 -0500

    Implemented gifted chat

--- components/Chat.js
+++ components/Chat.js
@@ -8,12 +8,16 @@ import {
   Alert,
   ScrollView,
 } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 let color = "teal";
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      messages: [],
+    };
   }
 
   componentDidMount() {
@@ -22,6 +26,27 @@ export default class Chat extends React.Component {
     let color = this.props.route.params.color;
 
     this.props.navigation.setOptions({ title: name });
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
   }
 
   render() {
@@ -31,6 +56,13 @@ export default class Chat extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hello, {name}! welcome to Chatter Box!</Text>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
       </View>
     );
   }
